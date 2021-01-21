9ff33d83940d67539736c11fa1399e71fca84d1a
guojiarui1102 <guojiarui@smail.nju.edu.cn>
2021-01-21 09:21:04 +0800

    update

--- password.go
+++ password.go
@@ -21,12 +21,12 @@ func (pw *password) toString() string {
 }
 
 // ParsePassword 解析获取密码
-func ParsePassword(pwString string) (*password, error) {
+func ParsePassword(pwString string) ([]byte, error) {
 	bs, err := base64.StdEncoding.DecodeString(strings.TrimSpace(pwString))
 	if err != nil || len(bs) != passwordLength {
 		return nil, errors.New("不符合规定的密码")
 	}
-	password := password{}
-	copy(password[:], bs)
-	return &password, nil
+	var pw []byte
+	copy(pw, bs)
+	return pw, nil
 }
